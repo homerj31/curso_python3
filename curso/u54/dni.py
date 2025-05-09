2bde1702c20af08c6f9aa26200ed9d2c544ad264
Oscar <obecerril.basalo@gmail.com>
2025-05-09 12:27:05 +0200

    arreglada identacion

--- curso/u54/dni.py
+++ curso/u54/dni.py
@@ -2,29 +2,29 @@
 
 class Dni():
 
-	def __init__(self,numero):
-		self.numero=numero
+    def __init__(self,numero):
+        self.numero=numero
 
-	def __calcular_letra(self):
-		letras = 'TRWAGMYFPDXBNJZSQVHLCKE'
-		return letras[int(self.numero)%23]
+    def __calcular_letra(self):
+        letras = 'TRWAGMYFPDXBNJZSQVHLCKE'
+        return letras[int(self.numero)%23]
 
-	@property
-	def numero(self):
-		return self._numero
+    @property
+    def numero(self):
+        return self._numero
 
-	@numero.setter
-	def numero(self,numero):
-		if len(numero)==8 and numero.isdigit():
-			self._numero = numero
-			self._letra = self.__calcular_letra()
-		else: 
-			raise ValueError("DNI incorecto")
+    @numero.setter
+    def numero(self,numero):
+        if len(numero)==8 and numero.isdigit():
+            self._numero = numero
+            self._letra = self.__calcular_letra()
+        else:
+            raise ValueError("DNI incorecto")
 
 
-	@property
-	def letra(self):
-		return self._letra
+    @property
+    def letra(self):
+        return self._letra
 
-	def __str__(self):
-		return "{0}-{1}".format(self.numero,self.letra)
+    def __str__(self):
+        return f"{self.numero}-{self.letra}"
